47a5501d9b10efba5950c434d5b582c2b7e8ed79
Molotov35 <emontejom1@miumg.edu.gt>
2022-06-16 14:33:43 -0600

    subiendo catalogo

---
+++ Catalogo.h
@@ -0,0 +1,14 @@
+
+
+
+class Registro
+{
+     private:
+        //string id,name,phone,address;
+     public:
+        void menu();
+        void altas();
+        void bajas();
+        void modificaciones();
+        void lectura();
+};
